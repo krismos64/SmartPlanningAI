61c5f3aa927189eb625ae10dce3e15dfd0ec5da9
krismos64 <c.mostefaoui@yahoo.fr>
2025-04-02 18:00:53 +0200

    new video

--- src/pages/LandingPage.js
+++ src/pages/LandingPage.js
@@ -1081,7 +1081,7 @@ const LandingPage = () => {
           />
           <DemoVideoContainer>
             <iframe
-              src="https://www.youtube.com/embed/bvvlO-FZuVU"
+              src="https://www.youtube.com/watch?v=W4UWkI4S2Qg&ab_channel=krismosDev"
               title="SmartPlanning - Démonstration du logiciel de planification intelligent sur smartplanning.fr"
               allow="accelerometer; autoplay; clipboard-write; encrypted-media; gyroscope; picture-in-picture"
               allowFullScreen
