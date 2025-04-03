9835e744aa04d26ca2ba87b6d228b07c81b83543
krismos64 <c.mostefaoui@yahoo.fr>
2025-04-03 13:53:56 +0200

    routes domain

--- backend/routes/csrfRoutes.js
+++ backend/routes/csrfRoutes.js
@@ -19,7 +19,7 @@ const handleCsrfToken = (req, res) => {
     sameSite: "None",
     httpOnly: false,
     path: "/",
-    domain: "smartplanning.onrender.com", // 🔥 Important : définit correctement le domaine pour que le cookie soit lisible côté client
+    domain: "smartplanning.fr", // 🔥 Important : définit correctement le domaine pour que le cookie soit lisible côté client
   });
 
   console.log("🔐 [CSRF] Token généré:", csrfToken.substring(0, 10) + "...");
